13b9ac097d92ea08b18ad3436b0fad106101e5d7
Uitumen <uitumen@hotmail.com>
2025-01-05 21:28:22 +0800

    add push notification support and periodic notifications in service worker; enhance service worker registration in index.html

--- service-worker.js
+++ service-worker.js
@@ -3,19 +3,16 @@ const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
-    'https://picsum.photos/200/300',
-    './path/to/icon.png',
+    './components/my-product.js',
     'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', (event) => {
+    // Precache assets on install
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(urlsToCache);
+    }));
 });
 
 self.addEventListener('fetch', event => {
@@ -43,4 +40,31 @@ self.addEventListener('activate', event => {
             );
         })
     );
+});
+
+self.addEventListener('push', event => {
+    const data = event.data ? event.data.json() : {};
+    const title = data.title || 'New Notification';
+    const options = {
+        body: data.body || 'You have a new notification.',
+        icon: './path/to/icon.png',
+        badge: './path/to/icon.png'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(title, options)
+    );
+});
+
+addEventListener('message', event => {
+    console.log("message handled");
+    if (event.data === 'showNotification') {
+        const title = 'Periodic Notification';
+        const options = {
+            body: 'This is a periodic notification.',
+            icon: './path/to/icon.png',
+            badge: './path/to/icon.png'
+        };
+        self.registration.showNotification(title, options);
+    }
 });
